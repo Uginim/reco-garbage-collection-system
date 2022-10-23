7db202b6516c3390e02a3d99d8f471b3412a641a
uginim <kimugiugi@gmail.com>
2022-10-24 01:34:30 +0900

    fix : 새사업장 등록 api 오류 수정
    기존에 작성하였던 날짜 포멧 확인용 어노테이션이 문제를일으킴. 현재 해당 어노테이션 삭제함

--- src/main/java/com/reco/garbagecollection/garbagecollectionsys/web/dto/SiteInfoDto.java
+++ src/main/java/com/reco/garbagecollection/garbagecollectionsys/web/dto/SiteInfoDto.java
@@ -25,7 +25,6 @@ public class SiteInfoDto {
     private String location; // 업장 지역
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @NotNull(message = "계약일자를 입력해주세요 yyyy-MM-dd")
-    @Pattern(regexp = "\\d{4}-(0\\d|1[0-2])-([0_2]\\d|3[0-1])", message = "날짜 형식을 지켜주세요 yyyy-MM-dd (년-월-일)")
     @ApiModelProperty(value = "계약시작일자", example = "2022-10-24", required = true)
     private LocalDate contractDate ; // 계약일자
     @NotBlank(message = "사업자명을 입력해주세요")
